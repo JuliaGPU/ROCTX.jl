10df37d5d7b9c2e725cc9030ea647a3832fe5db4
Carsten Bauer <carsten.bauer@uni-paderborn.de>
2023-08-01 11:03:02 +0200

    example

--- example/Cpp/compile.sh
+++
@@ -1,1 +0,0 @@
-hipcc roctx_test.cpp -o roctx_test -I/opt/rocm/include/roctracer/ -L/opt/rocm/lib -lroctx64
--- example/Cpp/profile.sh
+++
@@ -1,6 +0,0 @@
-rocprof --roctx-trace ./roctx_test
-
-# export HSA_TOOLS_REPORT_LOAD_FAILURE=1
-# export ROCTRACER_DOMAIN="roctx"
-# export LD_PRELOAD="/opt/rocm/lib/roctracer/libroctracer_tool.so"
-# ENABLE_JITPROFILING=1 rocprof --roctx-trace julia --project profile_instrumented.jl
